7eec1d9262f1c319d2ea143668057e80705ff3f0
maho0628 <maho06282001@gmail.com>
2025-06-14 23:57:39 +0900

    データ壊れ修正
    Former-commit-id: 65676e5eebe2f12e4b0e4d320fc1658dbf139c31

---
+++ Humanity-of-the-Dead/Assets/Yamashina/Script/Test.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]    SceneTransitionManager sceneTransitionManager;
+    [SerializeField] SceneInformation sceneInformation;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    //sceneTransitionManager = GetComponent<SceneTransitionManager>();    
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    //sceneTransitionManager.SceneChange(SceneInformation.SCENE.StageOne);
+    //    //sceneInformation.SetPreviousScene((SceneInformation.SCENE)SceneManager.GetActiveScene().buildIndex);
+
+    //}
+}
