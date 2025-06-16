f5f8ab1a28009f6b8b20fd4e769dc09f4014ca00
maho0628 <maho06282001@gmail.com>
2025-06-16 10:08:29 +0900

    Remove build files and fix push error
    Former-commit-id: 4ba74f38ebf4a1bdd2ba02d08d2281ece436ceaf

---
+++ Humanity-of-the-Dead/Assets/Yamashina/Script/MouseOverChange/ImageColorChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ImageColorChange : MonoBehaviour
+{
+    Image image;
+    [SerializeField] Sprite sprite_Unover;//マウスが触れていない時の画像
+    [SerializeField] Sprite sprite_over;//マウスオーバー時の画像
+
+
+
+
+    //マウスオーバー時に画像変更するメソッド
+    public void Changed()
+    {
+        image = GetComponent<Image>();
+        image.sprite = sprite_over;
+    }
+    //画像変更を元に戻す
+
+    public void Restart()
+    {
+        image = GetComponent<Image>();
+
+        Debug.Log(image.sprite);
+        Debug.Log(sprite_Unover);
+        image.sprite = sprite_Unover;
+       
+
+    }
+
+}
