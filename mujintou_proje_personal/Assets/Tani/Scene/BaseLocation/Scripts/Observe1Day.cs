1dda759030b295134db77fc8c96adca2d1d5a0cd
maho0628 <maho06282001@gmail.com>
2025-07-24 23:29:59 +0900

    Read me 追加

--- mujintou_proje_personal/Assets/Tani/Scene/BaseLocation/Scripts/Observe1Day.cs
+++ mujintou_proje_personal/Assets/Tani/Scene/BaseLocation/Scripts/Observe1Day.cs
@@ -8,6 +8,7 @@ public class Observe1Day : MonoBehaviour
     SceneObject op;
     private void Start()
     {
+        Debug.Log(PlayerInfo.Instance.Day.day.ToString());
         if(PlayerInfo.Instance.Day.day == 1)
         {
             PlayerInfo.Instance.DestroySelf();
